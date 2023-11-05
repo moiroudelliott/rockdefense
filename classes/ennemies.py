6d05b480d626e2beb1d86872deb88df54e43d668
FM04 <145712131+FM04@users.noreply.github.com>
2023-11-05 21:59:39 +0100

    modif ennemie

--- classes/ennemies.py
+++ classes/ennemies.py
@@ -61,57 +61,37 @@ class classique:
 
 
 class rapide:
-    def __init__(self, positionDepart):
+    def __init__(self, positionDepart, niveau):
         """
             positionDepart un tuple (x,y)
         """
 
         self.position = positionDepart
-        self.vie = 50
+        self.vie = 75
         self.sprite = ennemis2_img
-        self.degat = 15
-        self.vitesse = 3
-        self.resistance = {'physique': 0.75, 'magique': 0.75} #resistance naturelle
+        self.degat = 20
+        self.vitesse = 5
+        self.resistance = {'physique': 0.5, 'magique': 1} #resistance naturelle
         self.etat = {'buff': {'vitesse':1,'degat':1,'magique':1,'physique':1,},'debuff': {'vitesse':1,'magique':1,'physique':1,'degat':1,}}
         self.valeur = 10
+        self.pts = []
+        for pt in niveau.pts:
+            self.pts.append(r.randint(pt[0], pt[1]))
+        self.niveau = niveau
+        self.actualPt = 1
+
+        self.cooldown = 10
 
     def coefficientEtat(self, type):
-        return 1 * (self.etat['buff'][type] * self.etat['debuff'][type])
+        return 1 * (1/self.etat['buff'][type] * (self.etat['debuff'][type]) )
 
-    def update(self):
-        vit = self.vitesse
-        pos = self.position
-
-        deg = 0
-
-        if pos[0]<140:
-            pos[0] += vit
-        elif pos[1] > 80 and  pos[0]<350:
-            pos[1] -= vit
-        elif pos[0]<390:
-            pos[0] += vit
-        elif pos[1] < 610 and pos[0] < 650:
-            pos[1] += vit
-        elif pos[0] < 650:
-            pos[0] += vit
-        elif pos[1] > 40 and  pos[0]<655:
-            pos[1] -= vit
-        elif pos[0] < 1080:
-            pos[0] +=vit
-            pos[1] +=vit
-
-        else :
-            deg = self.degat_attaque()
-
-        self.position = pos
-        return deg
 
     def degat_inflige(self, degat, type):
 
         valeur_degat = degat
         type_degat = type
 
-        coefficient_resistance = self.resistance[type_degat] * (self.coefficientEtat(type_degat) + self.coefficientEtat(type_degat))
+        coefficient_resistance = (1/self.resistance[type_degat]) * self.coefficientEtat(type_degat)
         valeur_degat = valeur_degat * coefficient_resistance
 
         self.vie = self.vie - valeur_degat
@@ -122,66 +102,50 @@ class rapide:
         return self.degat * coefficient_degat
 
 
-    def display(self, CanvasParent, font):
+    def display(self, CanvasParent, font, timer):
 
-        self.update()
+        array = self.niveau.update(timer, self.vitesse, self.position, self.degat_attaque(), self.actualPt, self.pts, self.cooldown)
+        self.position = array[1]
+        self.actualPt = array[2]
 
         CanvasParent.blit(self.sprite, self.position)
         vie = font.render(str(self.vie), True, "red")
         CanvasParent.blit(vie, (self.position[0],self.position[1]-10))
+
+        return array[0]
 
 class tank:
-    def __init__(self, positionDepart):
+    def __init__(self, positionDepart, niveau):
         """
             positionDepart un tuple (x,y)
         """
 
         self.position = positionDepart
-        self.vie = 1000
+        self.vie = 150
         self.sprite = ennemis3_img
-        self.degat = 10
-        self.vitesse = 0.5
-        self.resistance = {'physique': 2, 'magique': 1.5} #resistance naturelle
+        self.degat = 15
+        self.vitesse = 1
+        self.resistance = {'physique': 2, 'magique': 1} #resistance naturelle
         self.etat = {'buff': {'vitesse':1,'degat':1,'magique':1,'physique':1,},'debuff': {'vitesse':1,'magique':1,'physique':1,'degat':1,}}
         self.valeur = 10
+        self.pts = []
+        for pt in niveau.pts:
+            self.pts.append(r.randint(pt[0], pt[1]))
+        self.niveau = niveau
+        self.actualPt = 1
+
+        self.cooldown = 10
 
     def coefficientEtat(self, type):
-        return 1 * (self.etat['buff'][type] * self.etat['debuff'][type])
+        return 1 * (1/self.etat['buff'][type] * (self.etat['debuff'][type]) )
 
-    def update(self):
-        vit = self.vitesse
-        pos = self.position
-
-        deg = 0
-
-        if pos[0]<140:
-            pos[0] += vit
-        elif pos[1] > 80 and  pos[0]<350:
-            pos[1] -= vit
-        elif pos[0]<390:
-            pos[0] += vit
-        elif pos[1] < 610 and pos[0] < 650:
-            pos[1] += vit
-        elif pos[0] < 650:
-            pos[0] += vit
-        elif pos[1] > 40 and  pos[0]<655:
-            pos[1] -= vit
-        elif pos[0] < 1080:
-            pos[0] +=vit
-            pos[1] +=vit
-
-        else :
-            deg = self.degat_attaque()
-
-        self.position = pos
-        return deg
 
     def degat_inflige(self, degat, type):
 
         valeur_degat = degat
         type_degat = type
 
-        coefficient_resistance = self.resistance[type_degat] * (self.coefficientEtat(type_degat) + self.coefficientEtat(type_degat))
+        coefficient_resistance = (1/self.resistance[type_degat]) * self.coefficientEtat(type_degat)
         valeur_degat = valeur_degat * coefficient_resistance
 
         self.vie = self.vie - valeur_degat
@@ -192,10 +156,14 @@ class tank:
         return self.degat * coefficient_degat
 
 
-    def display(self, CanvasParent, font):
+    def display(self, CanvasParent, font, timer):
 
-        self.update()
+        array = self.niveau.update(timer, self.vitesse, self.position, self.degat_attaque(), self.actualPt, self.pts, self.cooldown)
+        self.position = array[1]
+        self.actualPt = array[2]
 
         CanvasParent.blit(self.sprite, self.position)
         vie = font.render(str(self.vie), True, "red")
         CanvasParent.blit(vie, (self.position[0],self.position[1]-10))
+
+        return array[0]
