649e2be2613a00ea36bf4b595522a0f150a3c4be
FM04 <145712131+FM04@users.noreply.github.com>
2023-11-09 16:23:16 +0100

    Add files via upload

--- imports.py
+++ imports.py
@@ -1,10 +1,73 @@
 import classes.bullet as bullet
 import classes.button as button
 import classes.emplacement as emplacement
-import classes.ennemies as ennemies
+# import classes.ennemies as ennemies
 import classes.tours as tours
 import classes.niveau as niveau
 import classes.vague as vague
 from classes.importation.import_textures import *
 import pygame
 import math as m
+
+## Ajout ( Déplacmeent dans fonctions ? )
+
+import random as r
+import classes.Jennemies as ennemies
+import json
+
+# actual_level = niveau.Niveau1() # A enlever
+
+## Importation des données ennemies
+PATH_DATA_ENNEMIES = "./Data/ennemies.json"
+
+with open( PATH_DATA_ENNEMIES ) as data_file:
+  file_contents = data_file.read()
+
+parsed_json = json.loads(file_contents)
+
+def generateEnemie (data, actual_level, dictComportement = ennemies.Dict_Comportement): #actual_level -> Niveau
+
+    # listeCoordonneAleatoire = [0, r.randint(500, 670)]
+    # res = classe(listeCoordonneAleatoire, actual_level)
+
+    # print(data)
+    comportement = data['comportement']
+    # classe = ennemies.EnnemieModel()
+    classe = dictComportement.get(
+        comportement,
+        dictComportement['standart']
+    )
+
+    classe = classe()
+    classe.importationJSON( data )
+
+    listeCoordonneAleatoire = [0, r.randint(500, 670)]
+
+    classe.create( listeCoordonneAleatoire , actual_level )
+
+    return classe
+
+# Liste_ennemies = [ennemies.classique, ennemies.tank, ennemies.rapide]
+#
+# Liste_ennemies[0] = ennemies.EnnemieModel().importationJSON(parsed_json[0])
+
+Liste_ennemies = []
+
+for data in parsed_json:
+
+    # comportement = data['comportement']
+
+    # enn = ennemies.EnnemieModel()
+    # enn.importationJSON( data )
+    Liste_ennemies.append( data )
+
+## Rammasse miette
+miette = True
+
+if miette:
+    del PATH_DATA_ENNEMIES
+    del parsed_json
+    del file_contents
+    del data_file
+    del data
+    del miette
